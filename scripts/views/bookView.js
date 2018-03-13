7badccd49f9249d93347cc2f41a1484a9b4ab69c
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-03-12 17:40:28 -0700

    start view controller

--- scripts/views/bookView.js
+++ scripts/views/bookView.js
@@ -1,1 +1,27 @@
-'use strict'
+'use strict';
+
+(function(module) {
+    // What do you need (import or require) from prior modules?
+    const Book = module.Book;
+
+    const bookView = {};
+    
+    bookView.initIndexPage = () => {
+        bookView.loadBooks();
+        bookView.renderBook();
+    };
+    
+    bookView.loadBooks = () => {
+        Book.all.forEach(book => {
+            bookView.loadBook(book);
+        });
+    };
+
+    bookView.renderBook = book => {
+        $('#books').append(book.toHtml());
+    };
+
+    // What does your module export
+    module.bookView = bookView;
+
+})(window.app || (window.app = {}));
