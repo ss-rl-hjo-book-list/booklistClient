51d06a6dc7d1f27a88b18f51be6502631b6ede46
Ryan Lee <Renaskigi@protonmail.com>
2018-03-14 14:05:12 -0700

    fixed book add problem

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
     const Book = module.Book;
     const bookView = module.bookView;
     page('/home', () => Book.fetchAll(bookView.initIndexPage));
-    page('/books/:id', ctx => Book.fetchOne(ctx.params.id, bookView.initDetail));
     page('/books/new', () => bookView.initNew());
+    page('/books/:id', ctx => Book.fetchOne(ctx.params.id, bookView.initDetail));
 
     // page('*', () => page.redirect('/home'));
 
